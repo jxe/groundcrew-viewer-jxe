9a78b6c02ca382044b7a791efcba619b4b91f7f3
Joe Edelman <joe.edelman@gmail.com>
2008-11-13 19:10:01 -0500

    Preliminary support for landmark types.  (ltypes)

--- src_apps/mobilize_app/mobilize_app.js
+++ src_apps/mobilize_app/mobilize_app.js
@@ -62,6 +62,15 @@ Viewer.apps.mobilize = {
     }
   },
   
+  new_landmark: function(state) {
+    if (!logged_in) return Viewer.join_please();
+    $.template('#new_landmark_dialog').show_dialog(function(form){
+      Ajax.fetch('/gc/create_landmark', form, function(ev){
+        EventDb.add(ev);
+        Viewer.open(ev.landmark_tag || ev.item_tag);
+      });
+    });
+  },
   
   // category level stuff
   
