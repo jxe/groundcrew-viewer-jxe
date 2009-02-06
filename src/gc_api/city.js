f732bdf5ce03b95e2578a23d33108045fef7995b
marmorkuchen <marmorkuchen@kodeaffe.de>
2009-02-06 15:02:38 -0500

    Added check if a city has landmarks before mapping through an array of undefined.

--- src/gc_api/city.js
+++ src/gc_api/city.js
@@ -1,11 +1,10 @@
 City = {
-          
   landmarks_list2: function() {
     if (!Viewer.selected_city) return '';
     var landmarks = Landmarks.find("=city_id " + Viewer.selected_city);
+    if (!landmarks) return '';
     return landmarks.map(function(lm){
       return "<a href='#' item='"+ lm.item_tag +"'>" + lm.title + "</a>";
     }).join('');
-	}
-			
+  }
 };
