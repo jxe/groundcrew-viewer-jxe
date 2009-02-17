c3515e6a86d1f826253605557b0ad57c4dc25b88
Joe Edelman <joe.edelman@gmail.com>
2009-02-16 22:49:56 -0500

    Assignment editor UI mockup

--- components/organize_app/organize_app.js
+++ components/organize_app/organize_app.js
@@ -13,10 +13,24 @@ Viewer.apps.organize = {
       MapMarkers.open(state.item, $.template('#organize_landmark_iw').app_paint()[0], 16);
   },
   
+  display_assignment_editor: function(state) {
+    return MapMarkers.open(state.item, $.template('#assignment_editor').app_paint()[0], 16);
+  },
+  
+  build_pos_form_submitted: function(data, state) {
+    return MapMarkers.open(state.item, $.template('#assignment_editor').app_paint()[0], 16);
+  },
+  
   item_status: function(state)     { return "This agent is available."; },
   item_believesin: function(state) { return " "; },
   item_celebrates: function(state) { return " "; },
   item_helpwith: function(state)   { return " "; },
-  item_didrecent: function(state)  { return " "; }
+  item_didrecent: function(state)  { return " "; },
+  
+  everyone_will: function(state) {
+    return "Smile mischeviously/Make hand signals/Caress yourself/Hum quietly/Look mysterious".split('/').map(function(x){
+      return "<option>" + x + "</option>";
+    }).join('');
+  }
   
 };
