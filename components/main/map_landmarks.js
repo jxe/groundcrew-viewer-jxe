jxe/groundcrew-viewer-jxe

commit 8e409af03ff9b89a4dcf967a397da9abdd6d61fe

significant code reorg (sorry!)
- the new LiveHTML.widgets mechanism allows putting the javascript fill
  mechanisms for palettes near the palettes themselves

- palettes have been renamed for clarity and uniformity

- groundcrew_viewer has been busted up.  base_viewer is now just
  viewer.js

- templates.js has been busted up.  the various templates are now with
  the code that uses them

- palettes.html has been busted up, there is now a file for each palette

- components/map_layers has been merged into the new components/main,
  which contains javascript and html for everything that's always on the
  screen and is not a palette.. for instance, the map and the header
  bar.

- I am working towards making src/viewer into a "guts" directory that
  mostly people don't have to know about.  components/main and
  components/palettes will be where the action is.

components/main/map_landmarks.js
@@ -1,12 +1,12 @@
-LandmarkLayer = {
+MapLandmarks = {
   
   map_init: function(map) {
-    var mgr = LandmarkLayer.mgr = new MarkerManager(map, {maxZoom: 19});
+    var mgr = MapLandmarks.mgr = new MarkerManager(map, {maxZoom: 19});
     GEvent.addListener(map, "moveend", function() {
       var bounds = map.getBounds();
       var zoom = map.getBoundsZoomLevel(bounds);
-      if (zoom <= 9) return LandmarkLayer.off();
-      else LandmarkLayer.fetch_landmarks_in_bounds(bounds);
+      if (zoom <= 9) return MapLandmarks.off();
+      else MapLandmarks.fetch_landmarks_in_bounds(bounds);
     });
     GEvent.trigger(map, "moveend");
   },
@@ -21,10 +21,10 @@ LandmarkLayer = {
       $.each(data.photos, function(){
         if (Landmarks.id("p" + this.photo_id)) return;
         var x = this;
-        var lm = LandmarkLayer.lm_from_pano(x);
-        var marker = LandmarkLayer.marker_for_lm(lm);
-        LandmarkLayer.mgr.addMarker(marker, 0);
-        LandmarkLayer.mgr.addMarker(marker, Map.Gmap.getZoom());
+        var lm = MapLandmarks.lm_from_pano(x);
+        var marker = MapLandmarks.marker_for_lm(lm);
+        MapLandmarks.mgr.addMarker(marker, 0);
+        MapLandmarks.mgr.addMarker(marker, Map.Gmap.getZoom());
       });
     });
   },
@@ -33,14 +33,14 @@ LandmarkLayer = {
     if (!city_id || !Map.Gmap) return;
     var lms = Landmarks.in_city(city_id);
     $.each(lms, function(){
-      var marker = LandmarkLayer.marker_for_lm(this);
-      LandmarkLayer.mgr.addMarker(marker, 0);
-      LandmarkLayer.mgr.addMarker(marker, Map.Gmap.getZoom());
+      var marker = MapLandmarks.marker_for_lm(this);
+      MapLandmarks.mgr.addMarker(marker, 0);
+      MapLandmarks.mgr.addMarker(marker, Map.Gmap.getZoom());
     });
   },
   
   off: function() {
-    LandmarkLayer.mgr.clearMarkers();
+    MapLandmarks.mgr.clearMarkers();
   },
   
   lm_from_pano: function(x) {
@@ -55,7 +55,7 @@ LandmarkLayer = {
   },
   
   marker_for_lm: function(lm) {
-    var marker = new GMarker(new GLatLng(lm.lat, lm.lng), {icon: IconFactory.for_landmark(lm), title: lm.title});
+    var marker = new GMarker(new GLatLng(lm.lat, lm.lng), {icon: MapIcons.for_landmark(lm), title: lm.title});
     GEvent.addListener(marker, "click", function(){ Viewer.open(lm.item_tag); });
     lm.map_marker = marker;
     return marker;
@@ -63,4 +63,4 @@ LandmarkLayer = {
   
 }
 
-components.push(LandmarkLayer);
+components.push(MapLandmarks);

src/viewer/viewer.js
@@ -21,7 +21,7 @@ Viewer = {
       var city_id = tag.resource().city_id;
       return Viewer.go('/organize/your_personal_squad/City__' + city_id + '/' + tag);
     }
-    [Viewer.current_app, Viewer].dispatch('marker_clicked', tag, Viewer.current_app.state);
+    LiveHTML.dispatch('marker_clicked', tag, Viewer.current_app.state);
   },
   
   marker_clicked: function(tag) {
@@ -30,12 +30,6 @@ Viewer = {
 
   breadcrumb_change: function(new_url, state) {
     Viewer.go(new_url);
-  },
-
-  dispatch: function(method, args) {
-    var args = $.makeArray(arguments);
-    var method = args.shift();
-    return [Viewer.current_app, Viewer].dispatch(method, args[0], args[1], args[2], args[3]);
   },
   
   parse_url: function(url) {
@@ -55,7 +49,7 @@ Viewer = {
 
   go: function(url, form_data) {
     // adjust url
-    if (url[0] == '#') return Viewer.dispatch(url.slice(1), Viewer.current_app.state);
+    if (url[0] == '#') return LiveHTML.dispatch(url.slice(1), Viewer.current_app.state);
     Viewer.prev_loc = Viewer.loc;
     url = Viewer.loc = Viewer.parse_url(url);
     console.log('Viewer.go('+url+')');
@@ -78,7 +72,7 @@ Viewer = {
       var x = parts[i];
       if (state[label] != x || state.first) {
         state[label] = x;
-        Viewer.dispatch("set_" + label, x, state);
+        LiveHTML.trigger("set_" + label, x, state);
       }
       if (x) renderer = "show_" + label;
     });
@@ -94,7 +88,7 @@ Viewer = {
     
     // run renderer
     Viewer.rendered = false;
-    app[renderer] && app[renderer](state);
+    LiveHTML.dispatch(renderer, state);
     if (!Viewer.rendered) Viewer.render(renderer);
     
     // clean up
@@ -103,28 +97,14 @@ Viewer = {
   },
 
   breadcrumbs: function() {
-    var app_name = Viewer.current_app_name;
-    var app = Viewer.current_app;
-    var state = app.state;
-    var breadcrumbs = [];
-    var breadcrumb_url = '/' + app_name;
-
-    // push World
-    breadcrumbs.push(tag('option', {value:'/welcome', content:'World'}));
-
-    $.each(app.url_part_labels, function(i, label){
-      var x = state[label];
-      if (x) {
-        breadcrumb_url += "/" + x;
-        var breadcrumb_label = state[label + "_label"] || x;
-        var label_max = 25;
-        if (breadcrumb_label.length > label_max) {
-          breadcrumb_label = breadcrumb_label.slice(0, label_max) + ' ...';
-        }
-        breadcrumbs.push(tag('option', {value:breadcrumb_url, content:breadcrumb_label}));
-      }
-    });
-    return breadcrumbs.join(' ');
+    var state = Viewer.current_app.state;
+    var breadcrumb_url = '/' + Viewer.current_app_name;
+    return tag('option', {value:'/welcome', content:'World'}) + 
+      Viewer.current_app.url_part_labels.map(function(key){
+        if (!state[key]) return null;
+        breadcrumb_url += "/" + state[key];
+        return tag('option', {value:breadcrumb_url, content:state[key + "_label"] || state[key]});
+      }).compact().join('');
   },
 
   render: function(renderer) {
@@ -171,14 +151,10 @@ Viewer = {
     state.item_label = state.item_r.title;
   },
 
-      
-  // functions
-  
-  zoom_out: function(){ Viewer.go('/'); return false; },
-  
-  
-  // dyn fills
-  
+  join_please: function() {
+    $.facebox($('#join_fbox').html());
+  }, 
+    
   blank:       function(){ return ''; }
 
 };

components/main/current_item.js
@@ -1,0 +1,24 @@
+LiveHTML.widgets.push({
+  
+  item_thumb_url: function(state) { if (state.item_r && state.item_r.thumb_url) return state.item_r.thumb_url.gcify_url(); },
+  item_title:     function(state) { return state.item_r.title; },
+
+  item_ltypes:    function(state) {
+    ltypes = state.item_r.ltypes;
+    if (!ltypes) {
+      return 'unknown';
+    } else {
+      return ltypes.split(' ')[0];
+    }
+  },
+
+  item_description: function(state) {
+    description = state.item_r.description;
+    if (!description) {
+      return '';
+    } else {
+      return description;
+    }
+  }
+  
+});

components/main/header.js
@@ -1,0 +1,15 @@
+LiveHTML.widgets.push({
+
+  self_posx_pts: function(state) {
+    return CurrentUser.posx_pts;
+  },
+
+  self_posx: function(state) {
+    return CurrentUser.posx;
+  },
+
+  self_squadm: function(state) {
+    return CurrentUser.squadm;
+  }
+
+});

components/main/map_icons.js
@@ -1,4 +1,4 @@
-IconFactory = {
+MapIcons = {
   
   for_landmark: function(lm) {
     return $.extend(new GIcon(), {

components/organize_app/organize_app.js
@@ -51,7 +51,7 @@ Viewer.apps.organize = {
     return EventDb.events.map(function (ev) {
       if (ev.re != parent_annc_tag && ev.annc_tag != parent_annc_tag) return;
       Event.improve(ev);
-      return Templates.event.t(ev);
+      return EventsPalette.event_t.t(ev);
     }).compact().join('');
   }
 };

components/palettes/agents_palette.html
@@ -1,0 +1,6 @@
+<div class="rsidep palette tblack" id="people_palette" style="top: 40px; max-width: 600px">
+  <h2> Engage people </h2>
+  <div fill="agents_to_guide_all"></div>
+  <a class="btn" href="##more">+ get guided</a> &nbsp;
+  <a class="btn" href="##more">see more...</a>
+</div>

components/palettes/agents_palette.js
@@ -1,0 +1,11 @@
+LiveHTML.widgets.push({
+  
+  agents_to_guide6: function(state) {
+    return Tiles.agent_tile.tt(state.agents && state.agents.slice(0,6));
+  },  
+
+  agents_to_guide_all: function(state) {
+    return Tiles.agent_tile.tt(state.agents);
+  }  
+  
+});

commit fcc187fc0324168607dde5f2a06df6dfa92ca378

fix bugs introduced in 8e409af03ff

components/main/map_landmarks.js
@@ -32,6 +32,7 @@ MapLandmarks = {
   city_changed: function(city) {
     if (!city || !Map.Gmap) return;
     var lms = Landmarks.in_city(city);
+    if (!lms) return;
     $.each(lms, function(){
       var marker = MapLandmarks.marker_for_lm(this);
       MapLandmarks.mgr.addMarker(marker, 0);

src/viewer/viewer.js
@@ -89,7 +89,7 @@ Viewer = {
     
     // run renderer
     Viewer.rendered = false;
-    LiveHTML.dispatch(renderer, state);
+    if (app[renderer]) app[renderer](state);
     if (!Viewer.rendered) Viewer.render(renderer);
     
     // clean up
@@ -104,7 +104,7 @@ Viewer = {
       Viewer.current_app.url_part_labels.map(function(key){
         if (!state[key]) return null;
         breadcrumb_url += "/" + state[key];
-        return tag('option', {value:breadcrumb_url, content:state[key + "_label"] || state[key]});
+        return tag('option', {value:breadcrumb_url, content:(state[key + "_label"] || state[key]).trim()});
       }).compact().join('');
   },
 