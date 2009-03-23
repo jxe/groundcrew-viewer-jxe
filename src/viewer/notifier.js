2dfe222cb5e8828b865db6f75896fde980df9150
marmorkuchen <marmorkuchen@kodeaffe.de>
2009-03-23 15:03:44 -0400

    renamed 'a' to 'ev' in viewer's notifier mechanism.

--- src/viewer/notifier.js
+++ src/viewer/notifier.js
@@ -1,15 +1,15 @@
 Notifier = {
   
-  did_add_new_event: function(a) {
+  did_add_new_event: function(ev) {
     // we only notify on items that are latched to us.
-    if (!a.item || !a.item.latched_by || $w(a.item.latched_by).indexOf(CurrentUser.tag) < 0) return;
+    if (!ev.item || !ev.item.latched_by || $w(ev.item.latched_by).indexOf(CurrentUser.tag) < 0) return;
 
-    if (a.atype == 'report')      alert( a.actor_title + " reports: [["+ a.msg +"]]" );
-    if (a.atype == 'accepted')    alert( a.actor_title + " has accepted your assignment" );
-    if (a.atype == 'declined')    alert( a.actor_title + " has declined your assignment" );
-    if (a.atype == 'completed')   alert( a.item_title + " has completed your assignment" );
-    if (a.atype == 'appreciated') alert( a.actor_title + " has appreciated you" );
-    if (a.atype == 'blocked')     alert( a.actor_title + " has blocked you" );
+    if (ev.atype == 'report')      alert( ev.actor_title + " reports: [["+ ev.msg +"]]" );
+    if (ev.atype == 'accepted')    alert( ev.actor_title + " has accepted your assignment" );
+    if (ev.atype == 'declined')    alert( ev.actor_title + " has declined your assignment" );
+    if (ev.atype == 'completed')   alert( ev.item_title + " has completed your assignment" );
+    if (ev.atype == 'appreciated') alert( ev.actor_title + " has appreciated you" );
+    if (ev.atype == 'blocked')     alert( ev.actor_title + " has blocked you" );
   }
   
 };
