26fc1c3cfaaa5145534cf2b7409ad418d1969197
Joe Edelman <joe.edelman@gmail.com>
2009-07-02 19:44:18 -0400

    js for agent bio, plus some reformating

---
+++ app/helpers/items.js
@@ -0,0 +1,111 @@
+LiveHTML.widgets.push({
+
+  // item state:  latch status, problems, etc
+  
+  item_status: function() {
+    return This._item.availability_status;
+  },
+  
+  is_latched: function() {
+    return !This._item.latch.startsWith('unlatched');
+  },
+  
+  has_problem: function() {
+    return This._item.availability_status == 'problem';
+  },
+  
+  problem: function() {
+    return "i can't feel my feet!";
+  },
+  
+  agent_assignable: function() {
+    // return true;
+    return This._item.availability_status != 'inaccessible';
+  },
+
+  agent_latched_and_assigned: function() {
+    return This._item.availability_status == 'assigned';
+  },
+  
+  agent_unlatched: function() {
+    return true;
+  },
+  
+  
+  
+  // operational involvements
+
+  item_current_operation_title: function() {
+    if (This._item.latch.startsWith('unlatched')) return " ";
+    return This._item.latch.split(' ')[2].resource().title;
+  },
+  
+  item_current_assignment: function() {
+    if (This._item.latch.startsWith('unlatched')) return " ";
+    return This._item.latch.split(' ')[2].resource().body;
+  },
+  
+  agent_assign_prompt: function() {
+    if (This._item.availability_status == 'assigned') {
+      return "Change this agent's assignment?";
+    } else {
+      return "What would you like this agent to do?";
+    }
+  },
+  
+  
+  
+  // other data
+
+  agent_skills_as_lis: function() {
+    var skills = This._item.has || ' ';
+    return $w(skills).map(function(x){
+      return "<li>" + x + "</li>";
+    }).join(', ');
+  },
+  
+  upfors_as_lis: function() {
+    var upfor = This._item.upfor || ' ';
+    return $w(upfor).map(function(x){
+      return "<li>" + x + "</li>";
+    }).join(', ');
+  },
+  
+  answers: function() {
+    if (!This._item.answers) return '';
+    var answers = This._item.answers.split(/;; ?/);
+    return answers.map(function(x){
+      var parts = x.split(' ');
+      if (!parts[1]) return '';
+      var data = parts.shift().split('-');
+      var q = Questions[data[0]];
+      var answer = parts.join(' ');
+      var tstamp = $time(data[1]);
+      return '<h6 class="question">Q. '+q+'</h6><div class="answer">A. &ldquo;'+answer+'&rdquo;<span class="timestamp">'+tstamp+'</span></div>';
+    }).join('');
+  },
+  
+  
+  
+  
+  // masses of agents
+  
+  agents_count: function() {
+    return Agents.all.length;
+  }
+  
+  
+  // agents_to_guide6: function() {
+  //   return Tiles.agent_tile.tt(This.agents.slice(0,6));
+  // },
+  // 
+  // agents_to_guide_all: function() {
+  //   return Tiles.agent_tile.tt(This.agents.slice(0, 6*7));
+  // },
+  
+  // TODO: make dynamic
+  // item_squad: function() {
+  //   return "Demo Squad";
+  // },
+  
+});
--- app/tools/tools.js
+++ app/tools/tools.js
@@ -34,72 +34,10 @@ App.modes.dispatch = {
 
 App.tools.assign_agents = {
     
-  make_it_happen_form_submitted: function(data, state) {
+  make_it_happen_form_submitted: function(data) {
     Operation.assign(This.item, data.assign, function(operation){
       go('@' + operation.id);
     });
-  },
-  
-  
-  agent_assignable: function(state) {
-    // return true;
-    return This._item.availability_status != 'inaccessible';
-  },
-
-  agent_latched_and_assigned: function(state) {
-    return This._item.availability_status == 'assigned';
-  },
-  
-  agent_unlatched: function(state) {
-    return true;
-  },
-  
-  agent_assign_prompt: function() {
-    if (This._item.availability_status == 'assigned') {
-      return "Change this agent's assignment?";
-    } else {
-      return "What would you like this agent to do?";
-    }
-  },
-  
-  make_it_happen_form_submitted: function(data, state) {
-    Operation.assign(This.item, data.assign, function(operation){
-      // go('@' + operation.id);
-      go("mode=Connect;tool=view_events");
-    });
-  },
-  
-  // TODO: make dynamic
-  item_squad: function(state) {
-    return "Demo Squad";
-  },
-
-  item_status: function(state) {
-    return This._item.availability_status;
-  },
-
-  item_believesin: function(state) { 
-    if (!This._item.believesin) return " ";
-    return "believes in: <b>" + This._item.believesin.semisplit().join(', ')  + "</b>";
-  },
-
-  item_current_operation_title: function(state) {
-    if (This._item.latch.startsWith('unlatched')) return " ";
-    return This._item.latch.split(' ')[2].resource().title;
-  },
-  
-  item_current_assignment: function(state) {
-    if (This._item.latch.startsWith('unlatched')) return " ";
-    return This._item.latch.split(' ')[2].resource().body;
-  },
-  
-  is_latched: function(state) {
-    return !This._item.latch.startsWith('unlatched');
-  },
-
-  
-  item_celebrates: function(state) { return " "; },
-  item_helpwith: function(state)   { return " "; },
-  item_didrecent: function(state)  { return " "; }
+  }
   
 };
