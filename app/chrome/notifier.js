a2fda14de8d7f8b796df07c06b07e71b06731104
Noah Miller <noahhmiller@gmail.com>
2009-10-08 14:39:51 -0400

    Fixed notifier event type names and increased growl window life

--- app/chrome/notifier.js
+++ app/chrome/notifier.js
@@ -1,22 +1,26 @@
 Notifier = {
-  
+
   did_add_new_event: function(ev) {
     Event.improve(ev);
     // we only notify on items that are latched to us.
     if (!ev.re || ev.re.resource().architect != This.user.tag) return;
-    
+
     // don't report if we're watching the thing
     if (ev.re == This.item) return;
-    
-    if (ev.atype == 'report')      $.jGrowl( ev.actor_title + " reports: [["+ ev.msg +"]]" );
-    if (ev.atype == 'responded')   $.jGrowl( ev.actor_title + " responded: [["+ ev.msg +"]]" );
-    if (ev.atype == 'responded_yes')   $.jGrowl( ev.actor_title + " responded: [["+ ev.msg +"]]" );
-    if (ev.atype == 'responded_no')   $.jGrowl( ev.actor_title + " responded: [["+ ev.msg +"]]" );
-    if (ev.atype == 'accepted')    $.jGrowl( ev.actor_title + " has accepted your assignment" );
-    if (ev.atype == 'declined')    $.jGrowl( ev.actor_title + " has declined your assignment" );
-    if (ev.atype == 'completed')   $.jGrowl( ev.actor_title + " has completed your assignment" );
-    if (ev.atype == 'appreciated') $.jGrowl( ev.actor_title + " has appreciated you" );
-    if (ev.atype == 'blocked')     $.jGrowl( ev.actor_title + " has blocked you" );
+
+    if (ev.atype == 'reported')     Notifier.growl( ev.actor_title + " reports: [["+ ev.msg +"]]" );
+    if (ev.atype == 'answered')     Notifier.growl( ev.actor_title + " responded: [["+ ev.msg +"]]" );
+    if (ev.atype == 'answered_yes') Notifier.growl( ev.actor_title + " responded: [["+ ev.msg +"]]" );
+    if (ev.atype == 'answered_no')  Notifier.growl( ev.actor_title + " responded: [["+ ev.msg +"]]" );
+    if (ev.atype == 'accepted')     Notifier.growl( ev.actor_title + " has accepted your assignment" );
+    if (ev.atype == 'declined')     Notifier.growl( ev.actor_title + " has declined your assignment" );
+    if (ev.atype == 'completed')    Notifier.growl( ev.actor_title + " has completed your assignment" );
+    if (ev.atype == 'appreciated')  Notifier.growl( ev.actor_title + " has appreciated you" );
+    if (ev.atype == 'blocked')      Notifier.growl( ev.actor_title + " has blocked you" );
+  },
+
+  growl: function(msg, options){
+    $.jGrowl(msg, {life: 15*1000});
   }
-  
+
 };
