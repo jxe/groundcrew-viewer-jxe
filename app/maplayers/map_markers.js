4c8d99cc59bd5de717ccb8bc1f146983b484fd8d
Joe Edelman <joe.edelman@gmail.com>
2009-11-09 16:53:40 -0500

    fixes for map junk

--- app/maplayers/map_markers.js
+++ app/maplayers/map_markers.js
@@ -1,14 +1,31 @@
 MapMarkers = {
   
+  last_opened: null,
+  last_opened_type: null,
+  
+  type: function(thing) {
+    if (thing.startsWith('Op')) return 'operation';
+    else if (thing.startsWith('Landmark')) return 'landmarks';
+    else return 'agents';
+  },
+  
   window: function(tmpl, min_zoom) {
     var site = This.item;
-    var layer = 'landmarks';
-    var reopen = true;
-    if (This.item.startsWith('Op')) {
-      reopen = false;
+    var type = MapMarkers.type(site);
+    var layer = type;
+    
+    // resolve site
+    if (type == 'operation') {
       site = This._item.focii.split(' ')[0];
+      layer = MapMarkers.type(site);
     }
-    if (site.startsWith('Person')) layer = 'agents';
+    
+    // consider reopening same window?
+    var reopen = false;
+    if (This.item == MapMarkers.last_opened && MapMarkers.last_opened_type == type)
+      reopen = true;
+    MapMarkers.last_opened = This.item;
+    MapMarkers.last_opened_type = type;
     Map.site_open(layer, site, tmpl.app_paint()[0], reopen);
   }
   
