ea45819636ca2033dec1e5b18c5a8896390535cf
Noah Miller <noahhmiller@gmail.com>
2010-04-06 18:00:52 -0400

    hide floaty after map click

--- app/tools/tools.js
+++ app/tools/tools.js
@@ -2,6 +2,7 @@ App.tools.add_landmark = {
 
   map_clicked: function() {
     if (App.zoomed_out()) return alert("Please zoom to a city.");
+    go('tool=');
     map.openInfoWindow(This.click_latlng, $.template('#new_landmark').app_paint()[0]);
   }
 
@@ -11,6 +12,7 @@ App.tools.add_mission_landmark = {
 
   map_clicked: function() {
     if (App.zoomed_out()) return alert("Please zoom to a city.");
+    go('tool=');
     map.openInfoWindow(This.click_latlng, $.template('#new_mission_landmark').app_paint()[0]);
   }
 
@@ -20,6 +22,7 @@ App.tools.add_question_landmark = {
 
   map_clicked: function() {
     if (App.zoomed_out()) return alert("Please zoom to a city.");
+    go('tool=');
     map.openInfoWindow(This.click_latlng, $.template('#new_question_landmark').app_paint()[0]);
   }
 
