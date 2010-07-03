0d0cfaf7cb8264382d400173cc105474e6292641
Joe Edelman <joe.edelman@gmail.com>
2010-07-03 15:10:12 -0700

    generated sidebar content

--- app/helpers/user.js
+++ app/helpers/user.js
@@ -8,19 +8,24 @@ LiveHTML.widgets.push({
     return window.current_stream_desc || '';
   },
   
-  squads_dropdown: function() {
+  oilspill_sidebar_content: function() {
+    return "<h2>I am OilSpill</h2><ul><li>Hear me roar</li></ul>";
+  },
+  
+  sidebar_content: function() {
+    if (current_stream == 'oilspill') return oilspill_sidebar_content();
     if (!window.stream_names) window.stream_names = {};
     stream_names['demo'] = 'Demo Squad';
     if (!demo) stream_names['demo-' + current_stream] = 'Demo ' + stream_names[current_stream];
     var streams = $keys(window.stream_names);
     // if (streams.length == 0) return "<li>You have no other squads available.</li>";
     
-    return streams.sort().map(function(stream){
-      if (stream == current_stream) return '';
+    return '<h2>Your Squads</h2><ul>' + streams.sort().map(function(stream){
+      // if (stream == current_stream) return '';
       var name = stream_names[stream];
       var switch_url = "/" + stream + "/live/";
       return '<li href="'+switch_url+'">' + name + '</li>';
-    }).join('');
+    }).join('') + "</ul>";
   },
 
   self_name: function() {
