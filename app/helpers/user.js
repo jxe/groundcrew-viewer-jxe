d2731549e3d7ed7faf3e2469d1bf8e168a24ec20
Noah Miller <noahhmiller@gmail.com>
2010-09-02 11:01:48 -0400

    removed misleading donate link

--- app/helpers/user.js
+++ app/helpers/user.js
@@ -10,7 +10,7 @@ go.push({
   },
   
   youbox: function() {
-    if (typeof window.sms_remaining == "number") return "<b>"+window.sms_remaining+"</b> text messages remaining until we run out of money.  <a target='_' href='https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=NM3YQR6REFWHQ'>Donate.</a>";
+    if (typeof window.sms_remaining == "number") return "<b>"+window.sms_remaining+"</b> text messages remaining";
     else if (window.posx) return "You've organized <b>48</b> positive experiences.";
     else return "Join squads to mobilize positive action.";
   },
